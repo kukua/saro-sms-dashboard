494c70221d410b6d165423c77816942c96d1e75c
Acellam Guy <abiccel@yahoo.com>
2018-01-18 22:55:48 +0300

    Remove development configs

--- public_html/.environment/default.php
+++ public_html/.environment/default.php
@@ -3,7 +3,7 @@ define("ENVIRONMENT", "production");
 define("BASEURL", "-");
 
 /** Dashboard database **/
-define("MYSQL_USER", "sarosmsdashboard");
-define("MYSQL_PASS", "sarosmsdashboard@6");
-define("MYSQL_HOST", "192.168.43.183");
-define("MYSQL_DB",   "sarosmsdashboard");
+define("MYSQL_USER", "");
+define("MYSQL_PASS", "");
+define("MYSQL_HOST", "");
+define("MYSQL_DB",   "");
